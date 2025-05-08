3f1df5a40def261255ee6edecc20f715697764b3
xiaopang0808 <2901705231@qq.com>
2025-05-08 16:46:55 +0800

    Create Haffman_Tree.cpp

---
+++ Haffman_Tree.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+typedef char elemtype;
+typedef struct {
+    elemtype data;
+    double weight;
+    int parent;
+    int lchild;
+    int rchild;
+}HTnode;
+
+void CreatHT（HTnode HT[],int n0){
+
+}
+
+
+
+int main(){
+    
+}
+
+
+
+
